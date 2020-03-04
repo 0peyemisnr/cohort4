05a54ae2ec4727479a418ba54db5f0dae6693da2
MAC <marvinkays@gmail.com>
2020-03-04 11:06:54 -0700

    Completed 100c competency

--- 01-getting-started/src/scripts/syntax.js
+++ 01-getting-started/src/scripts/syntax.js
@@ -99,31 +99,72 @@ people: (array, num3) => {
     return array;
 },
 //loops | for loop
-//  forLoop:(num2) => {
-//  var sum=0;
-// for (var i = 0; i <= num2; i++) {
-//     sum = sum + i;
-//   };
-
-//  },
+ forLoop:(arr) => {
+     let newArr=[];
+     for(var i=0;i<arr.length;i++){
+newArr[i]=arr[i]+5;
+     }
+ return newArr[0];
+ },
 
 //loops | for-in loops
-checkNames:(person) => {
-    let person = {firstname:"John", lastname:"Doe", age:"25"};
-     return person;
-     let text = "";
-     let x;
-     for (x in person) {
-     text += person[x] + " "; 
-};
+checkPerson:(obj1) => {
+    var newLetters ='';
+    var x=''
+    for (x in obj1){
+      newLetters+=obj1[x]+' ';
+    }
+    return newLetters;
+     
+
 },
 //while loop
-function myFunction() {
-    let text = "";
-    let i = 0;
-    while (i < 5) {
-      text += "<br>The number is " + i;
-      i++;
-    };
+checkNumber: (range) => {
+    let arr = []
+    var i = 0;
+    while (i < range) {
+        arr.push(i);
+        i++;
+    }
+        return arr;
+    
+},
+
+//do while loop test
+reduce: (size) => {
+    let total = 0, i = 0;
+    do {
+        total += i;
+        i++;
+    } while (i <= size)
+    return total;;
+},
+
+// aForEachFunction: (xyz) => {
+    forEachLoop: (x) => {
+        x.forEach(myFunction)
+        // var counter=0;
+        function myFunction(item, index, arr) {
+            arr[index] = item*2;
+        };
+        return x;
+    },
+
+    //declare objects
+    arraysToObject: (name, value) => {
+        const myObject = {};
+        for (var step = 0; step < name.length; step++){
+            myObject[name[step]]=value[step];
+        }
+        return myObject;
+},
+
+//Lookup key to retrieve values
+lookupKey: (newObject) => {
+    // return newObject.buddha;
+    var myKey = Object.keys(newObject)[0];
+    return newObject[myKey];
+},
+}
 
 export default syntax;
--- 01-getting-started/src/scripts/syntax.test.js
+++ 01-getting-started/src/scripts/syntax.test.js
@@ -51,20 +51,56 @@ test ('update names of people', () => {
 });
 
 //for loop test
-// var sum = 0
-// test ('for loop to read numbers less than 5', () => {
-//        expect(syntax.forLoop(5)).toBe(15);
-//        expect(syntax.forLoop(10)).toBe(55);
-// });
+var sumArray = [1,2,3,4,5];
+var sumArray1 = [10,20,30,40,50];
+var sumArray2 = [100,200,300,400,500];
+var sumArray3 = ['a','b','c','d','e'];
+test ('for loop to read arrays', () => {
+       expect(syntax.forLoop(sumArray)).toBe(6);
+        expect(syntax.forLoop(sumArray1)).toBe(15);
+       expect(syntax.forLoop(sumArray2)).toBe(105);
+       expect(syntax.forLoop(sumArray3)).toBe('a5');
+});
 
 //for-in loops
-test ('for-in loop to read names', () => {
-    let person = [fname, lname, age];
-    expect(syntax.person(person)).toBe("John","Doe","25");
+var person = {
+    name:'Kay'
+    ,Height: '5ft'
+    ,weight:'500kg'};
+    
+//var myLetters1 = ['a1','b1','c1','d1','e1'];
+test ('for-in loop to check a persons name', () => {
+    expect(syntax.checkPerson(person)).toBe('Kay 5ft 500kg ');
+    //expect(syntax.checkPerson(weight)).toBe('500kg ');
 });
 
 //while loop test
-test ('while loop test to check number less than 5', () => {
-    let myFunction = [1 < 5];
-    expect(syntax.myFunction(function)).toBe("1","2","3","4");
+var i = 0;
+test ('while loop test to check number range', () => {
+    expect(syntax.checkNumber(5)).toEqual([0,1,2,3,4]);
+});
+
+//do while loop test
+test("The reduced numbers added up", () => {
+    expect(syntax.reduce(1)).toBe(1);
+    expect(syntax.reduce(10)).toBe(55);
+    expect(syntax.reduce(100)).toBe(5050);
+});
+
+//for each function test 
+test('does for each loop work', () => {
+    expect(syntax.forEachLoop([2,4,8])).toStrictEqual([4,8,16]);
+    expect(syntax.forEachLoop([7,11,50])).toStrictEqual([14,22,100]);
+});
+
+//declare objects
+test('convert 2 arrays to object', () => {
+    expect(syntax.arraysToObject(['a','b','c'],[1,2,3])).toStrictEqual({a:1,b:2,c:3});
+    expect(syntax.arraysToObject(['x','y','z'],[97,98,99])).toStrictEqual({x:97,y:98,z:99});
+});
+
+//Lookup key to retrieve values
+test('new object', () => {
+    expect(syntax.lookupKey({'buddha':108})).toBe(108);
+    expect(syntax.lookupKey({'foldpath':8})).toBe(8);
 });
