8274b675d414387bea382f7b15f4360b130b59ae
MAC <marvinkays@gmail.com>
2020-03-07 19:17:58 -0700

    First commit on css/html

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -7,6 +7,8 @@ isEven: (num) => {
     size: (num) => {
         if (num < 10) return "small";
         if (num < 20) return "medium";
+        if (num < 100) return "large"
+        if (num > 100) return "extra large"
         return "large";
     },
 
